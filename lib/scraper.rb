7906f0ef019dce7a1226edd9691f4f25893a76be
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-06-01 21:57:07 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,10 +1,31 @@
 require 'open-uri'
+
+require 'pry'
+
+require 'open-uri'
+require 'nokogiri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
+    st_hash = Hash.new
+    page = open(index_url).read
+    parsed_html = Nokogiri::HTML(page)
+    names = parsed_html.css(".student-name")
+    locations = parsed_html.css(".student-location")
+    names_array = []
+    names.each do |item|
+      names_array << item.text
+      #binding.pry
+    end
+    names_array.each do |student|
+      if !st_hash.include?(student)
+        st_hash 
+      end
+    end
     
+   return names_array
   end
 
   def self.scrape_profile_page(profile_url)
@@ -12,4 +33,6 @@ class Scraper
   end
 
 end
+
+
 
